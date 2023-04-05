781889a536dd893c907dcf62f02d70db8cecbff4
Andrey Smolko <andrey.smolko.dev@gmail.com>
2023-04-05 19:02:39 +0300

    feat: Add quasar project, move code in quasar

---
+++ quasar-project/src/components/NavigationPanel/NavigationPanel.vue
@@ -0,0 +1,74 @@
+<template>
+  <nav class="navigation-panel">
+    <li class="navigation-panel__item">
+      <a class="navigation__link" href="#">
+        <img
+          class="item__icon"
+          src="../../assets/icons/panel/home.svg"
+          alt="home"
+        />
+      </a>
+    </li>
+    <li class="navigation-panel__item">
+      <a class="navigation__link" href="#">
+        <img
+          class="item__icon"
+          src="../../assets/icons/panel/friends.svg"
+          alt="friends"
+        />
+      </a>
+    </li>
+    <li class="navigation-panel__item">
+      <a class="navigation__link" href="#">
+        <img class="item__icon" src="../../assets/icons/panel/add.svg" alt="chat" />
+      </a>
+    </li>
+    <li class="navigation-panel__item">
+      <a class="navigation__link" href="#">
+        <img
+          class="item__icon"
+          src="../../assets/icons/panel/likes.svg"
+          alt="likes"
+        />
+      </a>
+    </li>
+    <li class="navigation-panel__item">
+      <a class="navigation__link" href="#">
+        <img
+          class="item__icon"
+          src="../../assets/icons/panel/user.svg"
+          alt="user"
+        />
+      </a>
+    </li>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+@import '../../css/app.scss';
+.navigation-panel {
+  margin: 78px 0 39px 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px;
+  min-width: 350px;
+  max-height: 70px;
+  background-color: $secondary-color;
+  box-shadow: 0px 5px 30px rgba(123, 122, 122, 0.5);
+  border-radius: 10px;
+}
+.navigation-panel__item {
+  list-style: none;
+}
+.navigation__link {
+  padding: 0;
+  margin: 0;
+  border: none;
+}
+</style>
---
+++ quasar-project/src/components/UserMenu/UserMenuItem.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="user-menu__item">
+    <div class="icon__block">
+      <img v-if="icon" :src="getIconPath(icon)" class="item__icon" :alt="`${icon} `" />
+    </div>
+    <div class="user-menu__content">
+      <span class="item__text">{{ menuTitle }}</span>
+      <span class="item__counter">{{ countWishes }}</span>
+      <span v-if="status" class="item__counter">{{ status }} </span>
+      <img class="item__btn" src="../../assets/icons/usermenu/arrow-right.svg" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    countWishes: {
+      type: Number,
+      default: null
+    },
+    menuTitle: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+    status: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    getIconPath (iconName) {
+      try {
+        const iconItem = iconName.toLowerCase()
+        return new URL(
+          `../../assets/icons/usermenu/${iconItem}.svg`,
+          import.meta.url
+        ).href
+      } catch (err) {
+        console.log(`< iconName Error > :  ${err}`)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../css/app.scss';
+
+.user-menu__item {
+  width: 335px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  min-height: 55px;
+}
+.icon__block {
+  width: 40px;
+}
+.item__icon {
+  margin-left: 15px;
+}
+.user-menu__content {
+  display: flex;
+  flex-direction: row;
+  width: 295px;
+}
+.item__text {
+  padding-left: 10px;
+  text-align: left;
+  @include font('Inter', 16px, normal, 500, 18px, normal);
+}
+.item__counter {
+  padding-left: 15px;
+  @include font('Inter', 16px, normal, 500, 18px, 0.02em);
+  color: $color-light;
+}
+.item__btn {
+  margin-left: auto;
+  margin-right: 25px;
+}
+</style>
